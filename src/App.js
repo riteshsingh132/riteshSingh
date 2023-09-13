cc2a4150d6ba09ebd47c7abd1172d79861b89817
unknown <luckyritesh132@gmail.com>
2023-09-13 22:04:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Banner from "./components/Banner";
+import Navbarcomp from "./components/Navbarcomp";
+import "./App.css"
+import About from "./components/About";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Navbarcomp/>
+   <Banner/>
+   <About/>
+   </>
   );
 }
 
